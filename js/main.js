0de29e1c25250427c099ffb9683728c3c163e4e1
okapeeokapee <okapee420@gmail.com>
2020-08-26 21:10:48 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+{
+  const open= document.getElementById('open');
+  const close= document.getElementById('close');
+  const modal= document.getElementById('modal');
+  const mask= document.getElementById('mask');
+}
+
